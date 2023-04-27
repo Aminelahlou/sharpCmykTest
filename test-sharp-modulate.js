f524c80aa8e0e2666eeaabafd79ee9e299db9872
Amine Lahlou <aminelahlou5@gmail.com>
2023-04-27 19:48:45 +0100

    feature(tests): adding resize, modulate and composite tests

--- test-sharp-modulate.js
+++ test-sharp-modulate.js
@@ -6,20 +6,26 @@ const sharpInstance = sharp("./input/test-sharp-input.tif")
   .toColourspace("cmyk")
   .toFile("./output/test-sharp-modulate-output.tif");
 
-const sharpInstance = sharp("./input/test-sharp-input.tif")
+const sharpInstance2 = sharp("./input/test-sharp-input.tif")
   .pipelineColourspace("cmyk")
   .modulate({ hue: 10, saturation: 1, brightness: 1 })
   .toColourspace("cmyk")
   .toFile("./output/test-sharp-modulate_hue-output.tif");
 
-const sharpInstance = sharp("./input/test-sharp-input.tif")
+const sharpInstance3 = sharp("./input/test-sharp-input.tif")
   .pipelineColourspace("cmyk")
   .modulate({ hue: 0, saturation: 1.2, brightness: 1 })
   .toColourspace("cmyk")
   .toFile("./output/test-sharp-modulate_saturation-output.tif");
 
-const sharpInstance = sharp("./input/test-sharp-input.tif")
+const sharpInstance4 = sharp("./input/test-sharp-input.tif")
   .pipelineColourspace("cmyk")
   .modulate({ hue: 0, saturation: 1, brightness: 1.2 })
   .toColourspace("cmyk")
   .toFile("./output/test-sharp-modulate_brightness-output.tif");
+
+const sharpInstance5 = sharp("./input/test-sharp-input.tif")
+  .pipelineColourspace("cmyk")
+  .modulate({ hue: 360, saturation: 1, brightness: 1 })
+  .toColourspace("cmyk")
+  .toFile("./output/test-sharp-modulate_hue_identity-output.tif");
